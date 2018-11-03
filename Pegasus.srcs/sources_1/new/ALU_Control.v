f7dc962450ad84ab741579e204b342ee9f855ae8
AreegMostafa <44683594+AreegMostafa@users.noreply.github.com>
2018-11-04 00:07:40 +0200

    Update ALU_Control.v

--- Pegasus.srcs/sources_1/new/ALU_Control.v
+++ Pegasus.srcs/sources_1/new/ALU_Control.v
@@ -18,19 +18,19 @@
 `include "defines.v"
 
 module ALU_Control (
-    input [4:0] OpCode, 
-    input [14:12] Inst, 
+    input [`IR_opcode] OpCode, 
+    input [`IR_funct3] Inst, 
     input Inst30, 
-    output reg [3:0] ALU_Sel
+    output reg [`ALUSEL_SIZE] ALU_Sel
     );
     always @(*) begin
         case (OpCode)
-            `OPCODE_Arith_R     :       ALU_Sel = {Inst[14:12], Inst30};
-            `OPCODE_Arith_I     :       if(Inst[14:12] == `F3_SRL)
-                                            ALU_Sel = {Inst[14:12], Inst30};
+            `OPCODE_Arith_R     :       ALU_Sel = {Inst[`IR_funct3], Inst30};
+            `OPCODE_Arith_I     :       if(Inst[`IR_funct3] == `F3_SRL)
+                                            ALU_Sel = {Inst[`IR_funct3], Inst30};
                                         else 
-                                            ALU_Sel = {Inst[14:12], `ZEROs_1}; 
-            `OPCODE_Branch      :       ALU_Sel = {`F3_ADD, `ONEs_1};              
+                                            ALU_Sel = {Inst[`IR_funct3], `ZERO_1}; 
+            `OPCODE_Branch      :       ALU_Sel = {`F3_ADD, `ONE_1};              
             `OPCODE_LUI         :       ALU_Sel = `ALU_PASS;
             default             :       ALU_Sel = `DEFAULT_OP;
         endcase
