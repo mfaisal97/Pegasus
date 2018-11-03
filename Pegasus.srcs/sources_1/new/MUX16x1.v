3d1c2e4de99680d88ea6306a360ef3d26d740422
AreegMostafa <44683594+AreegMostafa@users.noreply.github.com>
2018-11-03 15:26:34 +0200

    Update MUX16x1.v

--- Pegasus.srcs/sources_1/new/MUX16x1.v
+++ Pegasus.srcs/sources_1/new/MUX16x1.v
@@ -1,6 +1,16 @@
 // file: MUX16x1.v
 // author: @melodyg
-
+/*******************************************************************
+*
+* Module: module_name.v
+* Project: Project_Name
+* Author: name and email
+* Description: put your description here
+*
+* Change history: 01/01/17 – Did something
+* 10/29/17 – Did something else
+*
+**********************************************************************/
 `timescale 1ns/1ns
 `include "defines.v"
 
