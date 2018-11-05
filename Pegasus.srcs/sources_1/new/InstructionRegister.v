153a56286d332819d3a4c1ee5ecd8adcc25ae0e6
AreegMostafa <44683594+AreegMostafa@users.noreply.github.com>
2018-11-05 22:16:17 +0200

    Update InstructionRegister.v

--- Pegasus.srcs/sources_1/new/InstructionRegister.v
+++ Pegasus.srcs/sources_1/new/InstructionRegister.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module InstructionRegister(input clk, input rst, input[31:0] word, input load, output reg [31:0] inst, output reg compressed, output reg ready, output reg DontLoad);
+module InstructionRegister(input clk, input rst, input[31:0] word, input load, output reg [31:0] inst, output reg compressed, output reg stall, output reg DontLoad);
     reg [47:0] IR;
     reg [1:0] counter;
     always @(posedge clk) begin
@@ -28,21 +28,25 @@ module InstructionRegister(input clk, input rst, input[31:0] word, input load, o
             inst = 32'd0;
             compressed = 1'b0;
             counter = 2'b00;
+            IR = 48'd0;
+            DontLoad = 0;
+            stall = 0;
         end
         else begin
            if(load) begin
-              if(ready)
+              if(~|IR[15:0]) 
                     IR[31:0] = word;
               else  
                     IR[47:16] = word;
               counter = 0;                     
            end
            else begin
-                compressed = ~(word[1]&word[0]);
+                compressed = ~(IR[1]&IR[0]);
                 inst = compressed? {16'd0,IR[15:0]}: IR[31:0];
-                {ready, IR} = compressed? {1'b0, 16'd0, IR[16:47]}: {1'b1, 32'd0, IR[31:47]};
-                counter = compressed? counter + compressed: 2'd2;
-                DontLoad = counter != 2'd2;
+                IR = compressed? {16'd0, IR[16:47]}: {32'd0, IR[31:47]};///not sure
+                counter = compressed? counter + compressed : 2'd2;
+                stall = counter == 2'b01 && ~(IR[16]&IR[17]);
+                DontLoad = counter != 2'd2 && (~|IR[16:47] && counter != 2'd1);
            end  
         end
     end
