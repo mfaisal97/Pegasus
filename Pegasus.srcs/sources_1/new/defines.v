64e3c13d3fd90c153e3d716570934fb2af707f67
Muhammad Faisal <mfaisal@aucegypt.edu>
2018-11-17 22:26:15 +0200

    Adding ebreak ecall mret support

--- Pegasus.srcs/sources_1/new/defines.v
+++ Pegasus.srcs/sources_1/new/defines.v
@@ -56,10 +56,13 @@
 `define     F3_LEAST_CSR_RC             2'b11
 `define     F3_LEAST_LOC                13:12
 `define     F3_Imm_BIT_LOC              14
+`define     F3_CSR_ALL_BIT              3'b0
 `define     CSR_ADDR_LOCATION           31:20
 `define     CSR_ADDR_SIZE               11:0
 `define     CSR_ADDR_ONE                12'b1
 `define     CSR_ADDR_ZERO               12'b1
+`define     CSR_ADDR_MRET               12'h302
+
 
 `define     BR_BEQ                 3'b000
 `define     BR_BNE                 3'b001
